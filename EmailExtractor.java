4564b371b76a7c375c8cf8f9d14993db9f565b29
becomingxdev <desai.dev742@gmail.com>
2025-07-02 01:07:42 +0530

    readme and java file

---
+++ EmailExtractor.java
@@ -0,0 +1,12 @@
+public class EmailExtractor {
+    public static void main(String[] args) {
+        
+        String email = "your.name@example.com";
+        int positionOfAt = email.indexOf("@");
+        String userName = email.substring(0, positionOfAt);
+        String domainName = email.substring(positionOfAt + 1);
+
+        System.out.println("Username: " + userName);
+        System.out.println("Domain: " + domainName);
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# JAVA
+
+So I am learning JAVA from basic to intermediate. Including practicing git.
