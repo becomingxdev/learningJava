e063c178a8fa20a8a2b615f7605712f307269f53
becomingxdev <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 14:18:43 +0530

    print, datatype, conditional done

---
+++ FirstJavaProgram.java
@@ -0,0 +1,15 @@
+public class FirstJavaProgram {
+
+    public static void main(String[] args) {
+        
+        int score = 69;
+
+        if (score > 100) {
+            System.out.println("You win a bonus!");
+        } else {
+            System.out.println("Keep trying!");
+        }
+
+    }
+    
+}
